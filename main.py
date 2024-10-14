5890eec56be2b2c5f4850c1200caaf65bc0ad779
ufukkazmaci03 <ufukkazmaci@gmail.com>
2024-10-14 20:12:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+gerceklerim = ['En sevdiğim ders matematiktir','Space Wars isimli bir proje yaptım','Yeni bir bilgisayar almayı düşünüyorum']
+
+rastgele_bir_bilgi = random.choice(gerceklerim)
+print("Gerçeklerimden biri:",rastgele_bir_bilgi)
